fbfa0d9c12cd4833b662dfafe379c1a309bd8e3b
Sudeep <sudeepbiswas02@gmail.com>
2021-06-19 21:24:14 +0530

    Update template

--- template/src/App.js
+++ template/src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import "./App.css";
 import React, { useContext } from "react";
 import { Web3Context } from "./web3";
@@ -9,25 +9,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {account == null ? (
-          <button onClick={connectWeb3}>Connect Web3</button>
-        ) : (
-          <div>
-            <p>Account: {account}</p>
-            <button onClick={logout}>Logout</button>
-          </div>
-        )}
+        <img src={logo} className="App-logo" alt="ethereum_logo" />
+        <div>
+          {account == null ? (
+            <button onClick={connectWeb3}>Connect Web3</button>
+          ) : (
+            <div>
+              <p>Account: {account}</p>
+              <button onClick={logout}>Logout</button>
+            </div>
+          )}
+        </div>
+        <br/>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ethereum.org/en/what-is-ethereum/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Explore Ethereum
         </a>
       </header>
     </div>
